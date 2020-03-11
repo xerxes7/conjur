120836d8c3d9a688cae4eaa2c84c3747d075aa2e
Ofira Burstein <ofira.burstein@cyberark.com>
2020-03-11 12:04:30 +0200

    102 create account demo

--- initializer.sh
+++ initializer.sh
@@ -2,43 +2,35 @@
 
 SCRIPT_LOG_FILE="/opt/conjur-server/log/OFIRA_FILE.txt"
 
-function recreate_account
+function create_account_from_other_pod
 {
-  DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
-  echo "$DATE: Starting Check Status" >> $SCRIPT_LOG_FILE
-  #sleep 30
-  DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
-  echo "$DATE: Running Check Status" >> $SCRIPT_LOG_FILE
+   rm -rf /run/conjur-api-key/account-command
+   DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
+   echo "$DATE: Create Account From Other Pod" >> $SCRIPT_LOG_FILE
+   while true
+   do
+     DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
+     echo "$DATE: Iterate Check Status - Begin" >> $SCRIPT_LOG_FILE
 
-  while true
-  do
-    DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
-    echo "$DATE: Iterate Check Status - Begin" >> $SCRIPT_LOG_FILE
+     if [ -f /run/conjur-api-key/account-command ]; then
+       RESULT=$(cat /run/conjur-api-key/account-command)
+       DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
+       break
+     fi
+     DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
+     echo "$DATE: Iterate Check Status - Continue" >> $SCRIPT_LOG_FILE
+     sleep 1
 
-    if [ -f /opt/conjur-server/log/conjur-server.log ]; then
-      RESULT=$(grep -c StatusController /opt/conjur-server/log/conjur-server.log)
-      DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
-      echo "$DATE: File Exists RESULT=[$RESULT]" >> $SCRIPT_LOG_FILE
-      if [ "$RESULT" = "0" ]; then
-	echo "$DATE: Log File not ready - waiting" >> $SCRIPT_LOG_FILE
-      else
-        echo "$DATE: Log File ready - break" >> $SCRIPT_LOG_FILE
-        break
-      fi
-    fi
-    DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
-    echo "$DATE: Iterate Check Status - Continue" >> $SCRIPT_LOG_FILE
-    sleep 2
-
-  done
-  DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
-  echo "$DATE: Check Status Done - Create Account" >> $SCRIPT_LOG_FILE
-  conjurctl account delete myaccount
-  conjurctl account create myaccount  |  grep "API key" | awk '{print $5}' > /run/conjur-api-key/accountFile
-  DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
-  echo "$DATE: Account Created" >> $SCRIPT_LOG_FILE
+   done
+   DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
+   echo "$DATE: Check Status Done - Create Account" >> $SCRIPT_LOG_FILE
+   conjurctl account delete $RESULT
+   conjurctl account create $RESULT  |  grep "API key" | awk '{print $5}' > /run/conjur-api-key/api-key
+   DATE=$(date +"%Y-%m-%d %H:%M:%S,%3N")
+   echo "$DATE: Account Created" >> $SCRIPT_LOG_FILE
+   rm -rf /run/conjur-api-key/account-command
 }
 
-recreate_account &
+create_account_from_other_pod &
 conjurctl server > /opt/conjur-server/log/conjur-server.log
 
