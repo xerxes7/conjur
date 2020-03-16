4deeb8bf8c43be55160093c5d061e1b64d09f147
ykislovich <yahalom.kislovich@cyberark.com>
2020-03-16 17:00:19 +0200

    temp fips branch

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,7 +2,15 @@
 
 class StatusController < ApplicationController
   def index
-    render 'index'
+    OpenSSL.fips_mode=true
+    render json: {
+       # error: {
+       #     OPENSSL_VERSION: OpenSSL::OPENSSL_VERSION,
+       #     OPENSSL_LIBRARY_VERSION: OpenSSL::OPENSSL_LIBRARY_VERSION
+       # }.compact
+       }
+    # }, status: :not_found
+    # render 'index'
   end
 
 end
