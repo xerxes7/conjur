d4918391e904822cbec7c67c4bfad9912fcd758f
Nahum Cohen <nahum.cohen@cyberark.com>
2020-03-26 09:52:09 +0200

    sprockets to work with FIPS

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,6 +27,18 @@ RSpec.configure do |config|
     Digest = OpenSSL::Digest # override the default Digest with OpenSSL::Digest
     OpenSSL.fips_mode = true
     ActiveSupport::Digest.hash_digest_class = OpenSSL::Digest::SHA1.new
+    Sprockets::DigestUtils.module_eval do
+      def digest_class
+        OpenSSL::Digest::SHA256
+      end
+    end
+
+    new_sprockets_config = {}
+    Sprockets.config.each do |key, val|
+      new_sprockets_config[key] = val
+    end
+    new_sprockets_config[:digest_class] = OpenSSL::Digest::SHA256
+    Sprockets.config = new_sprockets_config.freeze
   end
 
   config.before(:suite) do
